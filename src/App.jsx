d8ba18a76ef5ef7ffa1b0e1794dc649b5b3b61a6
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-20 17:19:51 +0100

    end of lesson 45 (alternative props syntaxes v2 - destructuring props and imported data for shorter syntax)

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,59 @@
-import reactImg from './assets/react-core-concepts.png';
-import {CORE_CONCEPTS} from './data.js'
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 
-const reactDescriptions = ["Fundamental", "Crucial", "Core", "Essential"]
+const reactDescriptions = ["Fundamental", "Crucial", "Core", "Essential"];
 
-function getRandomInt(){
-return Math.floor(Math.random()*reactDescriptions.length)
+function getRandomInt() {
+  return Math.floor(Math.random() * reactDescriptions.length);
 }
 
-
-function Header(){
-  const description = reactDescriptions[getRandomInt()]
-  return(
+function Header() {
+  const description = reactDescriptions[getRandomInt()];
+  return (
     <header>
       <img src={reactImg} alt="stylised atom" />
-    <h1>React Essentials</h1>
-    <p>{description} React concepts you will need for almost any app you are going to build!</p>
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
     </header>
-  )
+  );
 }
 
-function CoreConcept(props){
+// Using object destructuring with props
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
-  )
+  );
 }
 
 function App() {
   return (
     <div>
-      <Header/>
-          <main>
-    <section id="core-concepts">
-    <h2>Core Concepts</h2>
-    <ul>
-      <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-      <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
-      <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
-      <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
-
-    </ul>
-    </section>
-        
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            {/* Shorter, alternative prop syntax below */}
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
 }
 
 export default App;
- 
