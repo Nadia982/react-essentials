6c1f3ef217c820c7767b34a11d068170aef71ddf
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-21 23:24:25 +0100

    lesson 56 method 1 (rendering content conditionally using a ternary)

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import {useState} from "react";
+import { useState } from "react";
 
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import { CORE_CONCEPTS } from "./data.js";
 import TabButton from "./components/Header/TabButton.jsx";
-import {EXAMPLES}  from "./data.js";
+import { EXAMPLES } from "./data.js";
 
 function App() {
-//5 MIN 26 SECONDS
-// const stateArray = useState("Please click a button"). The line two lines below is a destructured version of this line.
-// the names "selectedTopic, setSelectedTopic" could be any names, but the names below reflect standard naming convention for useState.  
-// the second element in the array that we get back from useState will always be a function provided by React that can be used to update this state.
-// the function in the second element of the array returned by useState will also tel React that the component function must be executed again. 
-const [selectedTopic, setSelectedTopic] = useState("components")
+  //5 MIN 26 SECONDS
+  // const stateArray = useState("Please click a button"). The line two lines below is a destructured version of this line.
+  // the names "selectedTopic, setSelectedTopic" could be any names, but the names below reflect standard naming convention for useState.
+  // the second element in the array that we get back from useState will always be a function provided by React that can be used to update this state.
+  // the function in the second element of the array returned by useState will also tel React that the component function must be executed again.
+  const [selectedTopic, setSelectedTopic] = useState(null);
 
-  function handleSelect(selectedButton){
+  function handleSelect(selectedButton) {
     //selectedButton should be one of: "components", "JSX", "props" or "state", depending on which button is selected
     setSelectedTopic(selectedButton);
-    
   }
   return (
     <div>
@@ -40,18 +39,22 @@ const [selectedTopic, setSelectedTopic] = useState("components")
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-          <TabButton onSelect={()=> handleSelect("components")}>Components</TabButton>
-          <TabButton onSelect={()=> handleSelect("jsx")}>JSX</TabButton>
-          <TabButton onSelect={()=> handleSelect("props")}>Props</TabButton>
-          <TabButton onSelect={()=> handleSelect("state")}>State</TabButton>
-                 </menu>
-                 <div id="tab-content">
-                  <h3>{EXAMPLES[selectedTopic].title}</h3>
-                 <p>{EXAMPLES[selectedTopic].description}</p>
-                 <pre><code>
-                 {EXAMPLES[selectedTopic].code}
-                  </code></pre>
-                 </div>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {!selectedTopic ? <p>Please select a topic</p> : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
