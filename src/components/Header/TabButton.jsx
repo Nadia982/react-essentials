0f527392b075071f293af5c65e07918c0293ba99
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-22 11:57:53 +0100

    lesson 57 (dynamic styling with CSS)

--- src/components/Header/TabButton.jsx
+++ src/components/Header/TabButton.jsx
@@ -1,11 +1,11 @@
 
 //onSelect on the line below could be called anything (e.g. onClick or any other name we choose), 
 //unlike the "children" prop which is provided by React and must be named children. 
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
 
 
   return (
-    <li><button onClick={onSelect}>{children}</button></li>
+    <li><button className={isSelected? "active" : ""} onClick={onSelect}>{children}</button></li>
   )
 }
 
