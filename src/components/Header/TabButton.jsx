1d9cd60105dbc3480d016f777aee26354be76e21
Natalie Gillam <ngillam@hotmail.co.uk>
2024-08-23 12:01:15 +0100

    end of lesson 66 (forwarding props to wrapped elements)

--- src/components/Header/TabButton.jsx
+++ src/components/Header/TabButton.jsx
@@ -1,11 +1,10 @@
 
 //onSelect on the line below could be called anything (e.g. onClick or any other name we choose), 
 //unlike the "children" prop which is provided by React and must be named children. 
-export default function TabButton({children, onSelect, isSelected}) {
-
+export default function TabButton({children, isSelected, ...props}) {
 
   return (
-    <li><button className={isSelected? "active" : ""} onClick={onSelect}>{children}</button></li>
+    <li><button className={isSelected? "active" : ""} {...props}>{children}</button></li>
   )
 }
 
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const Section = ({title, children, id}) => {
+//the line below uses the rest property to collect all other props that might be received on the "Section" component and merge them together into a props object.
+const Section = ({title, children, ...props}) => {
   return (
-    <section id={id}>
+    // in the line below, the props collected using the rest operator are forwarded to the <section> element using the spread operator.  
+    <section {...props}>
         <h2>{title}</h2>
         {children}
     </section>
